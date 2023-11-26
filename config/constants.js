a95a7248117f774450cf8c9460ddbfd3175892d7
shivas1432 <shivashanker7337@gmail.com>
2023-11-26 00:00:00 +0000

    Add project structure and build configuration

---
+++ config/constants.js
@@ -0,0 +1,17 @@
+﻿// Netflix replica configuration constants
+export const API_CONFIG = {
+  BASE_URL: 'https://api.themoviedb.org/3',
+  IMAGE_BASE_URL: 'https://image.tmdb.org/t/p/w500',
+  BACKDROP_BASE_URL: 'https://image.tmdb.org/t/p/original'
+};
+
+export const NETFLIX_CATEGORIES = [
+  'Trending Now',
+  'Netflix Originals',
+  'Top Rated',
+  'Action Movies',
+  'Comedy Movies',
+  'Horror Movies',
+  'Romance Movies',
+  'Documentaries'
+];
